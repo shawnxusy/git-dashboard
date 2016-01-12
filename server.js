a36ec95db54b4d64a923f70b6e5cee0afcafd657
Shawn Xu <shawn.xu@samsung.com>
2016-01-11 17:40:54 -0800

    Followed tutorial before step 13.

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 // Babel ES6/JSX Compiler
 require('babel-register');
 
+var path = require('path');
+
+var mongoose = require('mongoose');
+var express = require('express');
 var swig  = require('swig');
 var React = require('react');
 var ReactDOM = require('react-dom/server');
 var Router = require('react-router');
-var routes = require('./app/routes');
-
-var express = require('express');
-var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+
+var routes = require('./app/routes');
+var config = require('./config');
+var Character = require('./models/character');
 
 var app = express();
 
@@ -19,6 +23,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(config.database);
+mongoose.connection.on('error', function() {
+  console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?');
+});
 
 app.use(function(req, res) {
   Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
@@ -36,6 +45,25 @@ app.use(function(req, res) {
   });
 });
 
-app.listen(app.get('port'), function() {
+
+  /**
+ * Socket.io stuff.
+ */
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var onlineUsers = 0;
+
+io.sockets.on('connection', function(socket) {
+  onlineUsers++;
+
+  io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+
+  socket.on('disconnect', function() {
+    onlineUsers--;
+    io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+  });
+});
+
+server.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
